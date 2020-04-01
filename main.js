4b49babe0e1799e183998613e3277b079579ae71
Aideen F <38634230+aideenf@users.noreply.github.com>
2020-04-01 21:40:33 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+
+$( '.friend-drawer--onhover' ).on( 'click',  function() {
+  
+  $( '.chat-bubble' ).hide('slow').show('slow');
+  
+});
+
+if ('serviceWorker' in navigator) 
+{
+   //window.alert("Your browser does support this messaging application which uses service workers");
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+else 
+{
+  console.log('ServiceWorker not in navigator');
+  window.alert("Your browser does no support this messaging application");
+}
